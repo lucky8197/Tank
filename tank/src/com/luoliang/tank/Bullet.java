206362482e47ffec872d3a6f287205c5214a7ee5
Lucky <1245052165@qq.com>
2019-07-21 15:07:15 +0800

    子弹换成图片

--- tank/src/com/luoliang/tank/Bullet.java
+++ tank/src/com/luoliang/tank/Bullet.java
@@ -1,6 +1,5 @@
 package com.luoliang.tank;
 
-import java.awt.Color;
 import java.awt.Graphics;
 
 /**
@@ -27,9 +26,20 @@ public class Bullet {
 		if (!live) {
 			tf.bullets.remove(this);
 		}
-		Color c = g.getColor();
-		g.setColor(Color.RED);
-		g.fillOval(x, y, WIDTH, HEIGHT);
+		switch (dir) {
+		case LEFT:
+			g.drawImage(ResourceMgr.bulletL, x, y, null);
+			break;
+		case UP:
+			g.drawImage(ResourceMgr.bulletU, x, y, null);
+			break;
+		case RIGHT:
+			g.drawImage(ResourceMgr.bulletR, x, y, null);
+			break;
+		case DOWN:
+			g.drawImage(ResourceMgr.bulletD, x, y, null);
+			break;
+		}
 		move();
 	}
 
