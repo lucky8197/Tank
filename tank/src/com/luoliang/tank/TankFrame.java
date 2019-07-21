ff29c718e9110d744ad2bd0288ae1451b4c0b8ab
Lucky <1245052165@qq.com>
2019-07-21 15:33:35 +0800

    加入敌方坦克

--- tank/src/com/luoliang/tank/TankFrame.java
+++ tank/src/com/luoliang/tank/TankFrame.java
@@ -18,8 +18,9 @@ import java.util.List;
 @SuppressWarnings("all")
 public class TankFrame extends Frame {
 
-	Tank myTank = new Tank(200, 200, Dir.DOWN, this);
+	Tank myTank = new Tank(200, 400, Dir.DOWN, this);
 	List<Bullet> bullets = new ArrayList<>();
+	List<Tank> tanks = new ArrayList<>();
 	public static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 	Image offscreenImage = null;
 
@@ -66,6 +67,10 @@ public class TankFrame extends Frame {
 		// 不能用foreach（内部iterator），因为paint(g)方法需要删除。
 		for (int i = 0; i < bullets.size(); i++) {
 			bullets.get(i).paint(g);
+		}
+
+		for (int i = 0; i < tanks.size(); i++) {
+			tanks.get(i).paint(g);
 		}
 
 //		for (Bullet b : bullets) {   //不能这样删除
